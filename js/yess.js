0a322be2be2abde7ac609559f3df2f5feaf79d69
Dmitrii Zagorodnev <dmitrii.zagorodnev@outlook.com>
2022-08-17 21:20:05 +0300

    Update yess.js

--- js/yess.js
+++ js/yess.js
@@ -12,7 +12,7 @@ function loaded() {
 	setInterval(loop, 260);
 }
 var x = 0;
-var titleText = ["|", "A|", "As|", "Ast|", "Asti|", "Astis|" ,"Astis |", "Astis &middot|", "Astis &middot C|", "Astis &middot Co|", "Astis &middot Cou|", "Astis &middot Coun|", "Astis &middot Count|", "Astis &middot Counte|", "Astis &middot Counter|", "Astis &middot Counter-|", "Astis &middot Counter-S|", "Astis &middot Counter-St|", "Astis &middot Counter-Str|", "Astis &middot Counter-Stri|", "Astis &middot Counter-Strike|", "Astis &middot Counter-Strike |", "Astis &middot Counter-Strike 1|", "Astis &middot Counter-Strike 1.|", "Astis &middot Counter-Strike 1.6|", "|"];
+var titleText = ["|", "F|", "F3|" ,"F3 |", "F3 &middot|", "F3 &middot C|", "F3 &middot Co|", "F3 &middot Cou|", "F3 &middot Coun|", "F3 &middot Count|", "F3 &middot Counte|", "F3 &middot Counter|", "F3 &middot Counter-|", "F3 &middot Counter-S|", "F3 &middot Counter-St|", "F3 &middot Counter-Str|", "F3 &middot Counter-Stri|", "F3 &middot Counter-Strike|", "F3 &middot Counter-Strike |", "F3 &middot Counter-Strike 1|", "F3 &middot Counter-Strike 1.|", "F3 &middot Counter-Strike 1.6|", "|"];
 
 function loop() {
 	document.getElementsByTagName("title")[0].innerHTML = titleText[x++ % titleText.length];
